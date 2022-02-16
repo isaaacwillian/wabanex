803793810c6cbd5ff5d0234ddacb8fdd3239a632
Isaac Willian <isaaacwillian@gmail.com>
2022-02-16 10:58:44 -0300

    Aula 2 - Completa

---
+++ lib/wabanex/user.ex
@@ -0,0 +1,26 @@
+defmodule Wabanex.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @fields [:email, :password, :name]
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields) #valida se todos os campos estão preenchidos
+    |> validate_length(:password, min: 6) #valida se a senha tem no min 6 caracteres
+    |> validate_length(:name, min: 2)
+    |> validate_format(:email, ~r/@/) #bem simples p ver se um email eh um email
+    |> unique_constraint([:email]) #ve se n já tem um email igual
+  end
+end
